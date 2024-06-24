8cd68a3cbbea7bc9e477260de6417b7b17cd4cd1
Hemant Kumar Yadav <55074926+hemantyadav8006@users.noreply.github.com>
2024-06-25 04:35:43 +0530

    all changes are done and site is good to go!

--- index.js
+++ index.js
@@ -25,3 +25,24 @@ function check(){
     newTab.target = "_blank";
     newTab.click();
 };
+
+
+
+const mainMenu = document.querySelector('.links');
+const openMenu = document.querySelector('.openMenu');
+const closeMenu = document.querySelector('.closeMenu');
+
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', hide);
+
+
+function show() {
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+
+
+function hide() {
+    mainMenu.style.top = '-100%'
+}
